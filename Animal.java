1f166cc6d43b16a28fcaef5f4d0e0c7f4f51bdf3
Andrew Metz <andrewmetz@Andrews-MacBook-Air.local>
2025-08-18 11:13:08 -0700

    Main Test of Animal

---
+++ Animal.java
@@ -0,0 +1,21 @@
+public class Animal {
+    private String symbol;
+
+    public Animal () {
+        symbol = "0";
+    }
+
+    public String getSymbol () {
+        return symbol;
+    }
+
+    public void setSymbol (String s) {
+        symbol = s;
+    }
+
+    public String toString () {
+        return symbol;
+    }
+
+    
+}
